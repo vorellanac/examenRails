0e5739b7dc629ff60452ba116a5757d90c3962cd
vorellana <orellana.castro.victor@gmail.com>
2016-10-18 21:00:11 -0300

    superman commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/batman'
 
+   get 'pages/superman'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
